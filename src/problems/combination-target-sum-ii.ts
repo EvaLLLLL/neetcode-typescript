06bd93152ec7c2e2365317bba2e3834fc1777a1e
lyh <liuyuhao0414@gmail.com>
2025-03-03 23:44:20 +0800

    problem: Combination Sum II

---
+++ src/problems/combination-target-sum-ii.ts
@@ -0,0 +1,29 @@
+export function combinationTargetSumIi(
+  candidates: number[],
+  target: number
+): number[][] {
+  let cur: number[] = []
+  let res: number[][] = []
+
+  function backtrack(i: number, t: number) {
+    if (t === 0) {
+      res.push([...cur])
+    } else if (t < 0 || i >= candidates.length) {
+      return
+    } else {
+      cur.push(candidates[i])
+      backtrack(i + 1, t - candidates[i])
+
+      cur.pop()
+
+      while (i + 1 < candidates.length && candidates[i] === candidates[i + 1]) {
+        i++
+      }
+      backtrack(i + 1, t)
+    }
+  }
+
+  backtrack(0, target)
+
+  return res
+}
